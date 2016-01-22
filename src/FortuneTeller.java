f72a2d48deaeb7c7124b6c0da9ccd5243c564b8c
Mrfuzzy <league@WTS9.attlocal.net>
2016-01-21 19:52:43 -0800

    Fortune Teller
    hg

---
+++ src/FortuneTeller.java
@@ -0,0 +1,129 @@
+
+// Copyright Wintriss Technical Schools 2013
+import java.applet.AudioClip;
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.awt.image.BufferedImage;
+import java.util.Random;
+
+import javax.imageio.ImageIO;
+import javax.swing.JApplet;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.SwingUtilities;
+
+public class FortuneTeller extends JPanel implements Runnable, MouseListener {
+
+	BufferedImage fortuneTellerImage;
+
+	int frameWidth = 970;
+	int frameHeight = 536;
+
+	FortuneTeller() throws Exception {
+
+		fortuneTellerImage = ImageIO.read(getClass().getResource("dude.jpg"));
+
+		addMouseListener(this);
+	}
+
+	static void begin() {
+		JOptionPane.showMessageDialog(null,
+				"Welcome user,  your goal is to find the secret location, your hint is 'He is a rich man'");
+	}
+
+	public void mousePressed(MouseEvent e) {
+		int mouseX = e.getX();
+		int mouseY = e.getY();
+
+		System.out.println(mouseY + "y," + mouseX + "x");
+
+		int secretLocationX = 536;
+		int secretLocationY = 335;
+		/**
+		 * If the mouse co-ordinates and secret location are close, we'll let
+		 * them ask a question.
+		 */
+		if (areClose(mouseX, secretLocationX) && areClose(mouseY, secretLocationY)) {
+
+			JOptionPane.showInputDialog(null, "Ask me a question my boy...(or girl)");
+
+			AudioClip sounds = JApplet.newAudioClip(getClass().getResource("llama.wav"));
+
+			sounds.play();
+			pause(1);
+			// 11. Use the pause() method below to wait until your music has
+			// finished
+
+			// 12. Insert your completed Magic 8 ball recipe
+			// (http://bit.ly/Zdrf6d) here
+
+		}
+
+	}
+
+	private boolean areClose(int mouseX, int secretLocationX) {
+		return mouseX < secretLocationX + 15 && mouseX > secretLocationX - 15;
+	}
+
+	private void pause(int seconds) {
+		try {
+			Thread.sleep(1000 * seconds);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+	}
+
+	/****************
+	 * don't worry about the stuff under here
+	 *******************/
+
+	public static void main(String[] args) throws Exception {
+		SwingUtilities.invokeLater(new FortuneTeller());
+		begin();
+	}
+
+	@Override
+	public void run() {
+		JFrame frame = new JFrame();
+		frame.add(this);
+		setPreferredSize(new Dimension(frameWidth, frameHeight));
+		frame.pack();
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setResizable(false);
+		frame.setVisible(true);
+	}
+
+	private void showAnotherImage(String imageName) {
+		try {
+			fortuneTellerImage = ImageIO.read(getClass().getResource(imageName));
+		} catch (Exception e) {
+			System.err.println("Couldn't find this image: " + imageName);
+		}
+		repaint();
+	}
+
+	@Override
+	public void paintComponent(Graphics g) {
+		g.drawImage(fortuneTellerImage, 0, 0, null);
+	}
+
+	@Override
+	public void mouseClicked(MouseEvent e) {
+	}
+
+	@Override
+	public void mouseReleased(MouseEvent e) {
+	}
+
+	@Override
+	public void mouseEntered(MouseEvent e) {
+	}
+
+	@Override
+	public void mouseExited(MouseEvent e) {
+	}
+
+}
